9946187cab4e9867331c6419685e90111c130bd3
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2013-06-15 21:00:11 +0200

    Mock specific database (PDO)

--- tests/config/mock-database.php
+++ tests/config/mock-database.php
@@ -2,14 +2,19 @@
 use PHPUnit_Framework_MockObject_Generator as Mock;
 
 $any = new PHPUnit_Framework_MockObject_Matcher_AnyInvokedCount;
-$mockResult = Mock::getMock('Zend\Db\Adapter\Driver\ResultInterface');
 
-$mockStatement = Mock::getMock('Zend\Db\Adapter\Driver\StatementInterface');
+$mockPdoStatement = Mock::getMock('PDOStatement');
+
+$mockResult = Mock::getMock('Zend\Db\Adapter\Driver\Pdo\Result');
+$mockResult->expects($any)->method('getResource')->will(new PHPUnit_Framework_MockObject_Stub_Return($mockResult));
+
+$mockStatement = Mock::getMock('Zend\Db\Adapter\Driver\Pdo\Statement');
 $mockStatement->expects($any)->method('execute')->will(new PHPUnit_Framework_MockObject_Stub_Return($mockResult));
 
-$mockConnection = Mock::getMock('Zend\Db\Adapter\Driver\ConnectionInterface');
+$mockConnection = Mock::getMock('Zend\Db\Adapter\Driver\Pdo\Connection');
+$mockConnection->expects($any)->method('getConnectionParameters')->will(new PHPUnit_Framework_MockObject_Stub_Return(array('db' => array('driver' => 'pdo_pgsql'))));
 
-$mockDriver = Mock::getMock('Zend\Db\Adapter\Driver\DriverInterface');
+$mockDriver = Mock::getMock('Zend\Db\Adapter\Driver\Pdo\Pdo', array(), array($mockConnection));
 $mockDriver->expects($any)->method('createStatement')->will(new PHPUnit_Framework_MockObject_Stub_Return($mockStatement));
 $mockDriver->expects($any)->method('getConnection')->will(new PHPUnit_Framework_MockObject_Stub_Return($mockConnection));
 
